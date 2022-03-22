611ae97d75e25bd0350a5c0a1d36e694df3f8b7a
LearningHANG <299924@university-365.com>
2022-03-22 18:54:53 +0800

    Load planets data in tests

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,10 +1,12 @@
 const request = require('supertest');//用来测试http
 const app = require('../../app');
 const {mongoConnect,mongoDisconnect}=require('../../services/mongo');
+const {loadPlanetsData}=require('../../models/planet.model')
 
 describe('Launches API',()=>{
     beforeAll(async ()=>{
         await mongoConnect();
+        await loadPlanetsData();
     })
     afterAll(async ()=>{
         await mongoDisconnect();
